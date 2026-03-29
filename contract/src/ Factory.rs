2fda2a1fcd9814cbb9e6ecdbb261da2352f2eeb3
Jimmy W Moore <scaredfallen100@gmail.com>
2026-03-29 09:18:41 -0500

    Update  Factory.rs

--- contract/src/ Factory.rs
+++ contract/src/ Factory.rs
@@ -14,6 +14,8 @@ use near_sdk::serde::{Serialize, Deserialize};
 pub struct TokenProfile {
     pub owner: AccountId,
     pub total_supply: u128,
+    pub oim_enabled: bool,
+    pub modules: Vec<u8>,
 }
 
 #[near_bindgen]
@@ -22,22 +24,23 @@ pub struct Contract {
     tokens: UnorderedMap<String, TokenProfile>,
 }
 
-impl Default for Contract {
-    fn default() -> Self {
+#[near_bindgen]
+impl Contract {
+    #[init]
+    pub fn new() -> Self {
         Self {
             tokens: UnorderedMap::new(b"t"),
         }
     }
-}
 
-#[near_bindgen]
-impl Contract {
     pub fn create_token(&mut self, token_id: String, total_supply: u128) {
-        let owner = env::predecessor_account_id();
+        let caller = env::predecessor_account_id();
 
         let profile = TokenProfile {
-            owner,
+            owner: caller,
             total_supply,
+            oim_enabled: true,
+            modules: vec![1, 2, 3],
         };
 
         self.tokens.insert(token_id, profile);
