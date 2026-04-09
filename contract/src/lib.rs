3953c981e495c2be29221904afc7af25db0d9952
Jimmy W Moore <scaredfallen100@gmail.com>
2026-04-08 21:53:22 -0500

    Change owner type from String to AccountId

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,18 +1,19 @@
-use near_sdk::near;
+use near_sdk::{near, AccountId, PanicOnDefault};
 
 #[near(contract_state)]
+#[derive(PanicOnDefault)]
 pub struct Contract {
-    owner: String,
+    owner: AccountId,
 }
 
 #[near]
 impl Contract {
     #[init]
-    pub fn new(owner: String) -> Self {
+    pub fn new(owner: AccountId) -> Self {
         Self { owner }
     }
 
-    pub fn get_owner(&self) -> String {
+    pub fn get_owner(&self) -> AccountId {
         self.owner.clone()
     }
 }
