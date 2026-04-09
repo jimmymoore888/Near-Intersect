2150f7c858093bf91bf4dc92c9a3cd61a6de04b4
Jimmy W Moore <scaredfallen100@gmail.com>
2026-04-08 21:21:24 -0500

    Refactor near_bindgen and update attributes

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,19 +1,18 @@
 use near_sdk::{
     near,
-    near_bindgen,
     AccountId,
     PanicOnDefault,
-    borsh::{BorshDeserialize, BorshSerialize},
+    borsh::{self, BorshDeserialize, BorshSerialize},
 };
 
-#[near]
+#[near(contract_state)]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 #[borsh(crate = "near_sdk::borsh")]
 pub struct Contract {
     owner: AccountId,
 }
 
-#[near_bindgen]
+#[near]
 impl Contract {
     #[init]
     pub fn new(owner: AccountId) -> Self {
